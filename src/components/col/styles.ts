a539bc2c224aaf0c1a05685d6a29f722696b4254
melisademirparmak <melisademirparmaak@gmail.com>
2022-05-30 20:14:22 +0300

    create: col

---
+++ src/components/col/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledCol = styled.div<{
+    justifyContent?: string;
+    alignItems?: string;
+    flex?: number;
+
+}>`
+flex: ${({ flex }) => flex};
+align-items: ${({ alignItems }) => alignItems};
+justify-content: ${({ justifyContent }) => justifyContent};
+flex-direction: column;
+`;
