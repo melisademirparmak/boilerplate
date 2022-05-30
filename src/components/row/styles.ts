5e395c681f563c0a57e92072da24b46903e6dada
melisademirparmak <melisademirparmaak@gmail.com>
2022-05-30 20:33:44 +0300

    create: row

---
+++ src/components/row/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledRow = styled.div<{
+    justifyContent?: string;
+    alignItems?: string;
+    flex?: number;
+
+}>`
+flex: ${({ flex }) => flex};
+align-items: ${({ alignItems }) => alignItems};
+justify-content: ${({ justifyContent }) => justifyContent};
+flex-direction: row;
+display: flex;
+
+`;
